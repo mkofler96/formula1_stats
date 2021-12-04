02ac4d0af69d44a515bc3d91a4b13f27e514aa79
mkofler96 <rasta.kof@gmail.com>
2021-12-04 23:03:14 +0100

    experimental

--- backend.py
+++ backend.py
@@ -1,15 +1,70 @@
 from matplotlib import pyplot as plt
 import fastf1 as ff1
-from fastf1 import plotting
 import os
+import pandas as pd
+from fastf1.core import Laps
 
 def setup():
-    plotting.setup_mpl()
     ff1.Cache.enable_cache(os.getcwd())
 
-def rest():
-    monza_quali = ff1.get_session(2021, 'Monza', 'FP2')
 
+def load_summary(year, circuit):
+    fp1 = create_summary(ff1.get_session(year, circuit, 'FP1'))
+    fp2 = create_summary(ff1.get_session(year, circuit, 'FP2'))
+    fp3 = create_summary(ff1.get_session(year, circuit, 'FP3'))
+    return fp1, fp2, fp3
+
+
+def create_summary(session):
+    print("creating summary")
+    laps = session.load_laps()
+    drivers = pd.unique(laps['Driver'])
+    list_fastest_laps = list()
+    for drv in drivers:
+        drvs_fastest_lap = laps.pick_driver(drv).pick_fastest()
+        list_fastest_laps.append(drvs_fastest_lap)
+    o_laps = Laps(list_fastest_laps).sort_values(by='LapTime').reset_index(drop=True)
+    o_laps = o_laps[['Driver', 'LapTime', 'Compound']]
+
+    o_laps['LapTime'] = o_laps['LapTime'].apply(lambda x: strfdelta(x))
+    return o_laps
+
+
+def strfdelta(tdelta):
+    cmp = tdelta.components
+    time_string = str(cmp[2])+":"+str(cmp[3])+":"+str(cmp[4])
+    return time_string
+
+
+def load_laps(year, circuit, fp1, fp2, fp3):
+    print("loading laps")
+    fp1 = create_heat_map(ff1.get_session(year, circuit, 'FP1').load_laps(), fp1['Driver'])
+    fp2 = create_heat_map(ff1.get_session(year, circuit, 'FP2').load_laps(), fp2['Driver'])
+    fp3 = create_heat_map(ff1.get_session(year, circuit, 'FP3').load_laps(), fp3['Driver'])
+    return fp1, fp2, fp3
+
+
+def create_heat_map(laps, drivers):
+    list_driver_laps = list()
+    for drv in drivers:
+        drvs_laps = laps.pick_driver(drv).reset_index()
+        float_laptime = drvs_laps["LapTime"].apply(lambda x: float_tdelta(x))
+        list_driver_laps.append(float_laptime)
+    heatmap = pd.concat(list_driver_laps, axis=1)
+    heatmap.columns = drivers
+    return heatmap
+
+def float_tdelta(tdelta):
+    try:
+        cmp = tdelta.components
+        time = cmp[2]*60 + cmp[3] + cmp[4]/1000
+    except:
+        time = float("inf")
+    return time
+
+
+
+def rest():
     laps = monza_quali.load_laps(with_telemetry=True)
     fast_leclerc = laps.pick_driver('LEC').pick_fastest()
     lec_car_data = fast_leclerc.get_car_data()
--- main.py
+++ main.py
@@ -4,10 +4,87 @@ import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib as mtpltlib
-
-
+from datetime import datetime as dt
+import seaborn as sns
 from backend import *
 
-setup()
-st.write("setup done")
+year = 2021
+circuit = "qatar"
+
+#setup()
+#fp1, fp2, fp3 = load_summary(year, circuit)
+#laps_fp1, laps_fp2, laps_fp3 = load_laps(year, circuit, fp1, fp2, fp3)
+
+# layout
+#st.set_page_config(layout="wide")
+#null, col_left_fp1, col_right_fp1, null2 = st.columns([0.5, 1, 2.5, 0.5])
+#null, col_left_fp2, col_right_fp2, null2 = st.columns([0.5, 1, 2.5, 0.5])
+#null, col_left_fp3, col_right_fp3, null2 = st.columns([0.5, 1, 2.5, 0.5])
+#col_left_fp1.dataframe(data = fp1, width=1024, height=768)
+#col_left_fp2.dataframe(data = fp2, width=1024, height=768)
+#col_left_fp3.dataframe(data = fp3, width=1024, height=768)
+
+#set background
+#st.markdown(
+#    """
+#    <style>
+#    .reportview-container {
+#        background: url("https://images5.alphacoders.com/317/thumb-1920-317664.jpg")
+#    }
+#    </style>
+#    """,
+#    unsafe_allow_html=True
+#)
+
+#sns.set_style('white')
+# Draw a heatmap with the numeric values in each cell
+f, ax = plt.subplots()
+#plt.grid(visible=None)
+
+#sns.heatmap(laps_fp1.transpose(), vmin=laps_fp1.min().min(), vmax=laps_fp1.min().max(), fmt="f", linewidths=.5, ax=ax, mask=laps_fp1.transpose().isnull())
+#col_right_fp1.pyplot(f)
+
+f2, ax2 = plt.subplots()
+
+#sns.heatmap(laps_fp2.transpose(), vmin=laps_fp2.min().min(), vmax=laps_fp2.min().max(), fmt="f", linewidths=.5, ax=ax2, mask=laps_fp2.transpose().isnull())
+#col_right_fp2.pyplot(f2)
+
+f3, ax3 = plt.subplots()
+#sns.heatmap(laps_fp3.transpose(), vmin=laps_fp3.min().min(), vmax=laps_fp3.min().max(), fmt="f", linewidths=.5, ax=ax3, mask=laps_fp3.transpose().isnull())
+#col_right_fp3.pyplot(f3)
+def SIR(y, t, N, beta, gamma):
+    S, I, R = y
+    dSdt = -beta * S * I / N
+    dIdt = beta * S * I / N - gamma * I
+    dRdt = gamma * I
+    return dSdt, dIdt, dRdt
+
+N = 1000
+beta = 1.0
+D = 4.0
+gamma = 1.0 / D
+
+S0, I0, R0 = 999, 1, 0
+
+t = np.linspace(0, 49, 50)
+S, I, R = t, t, t
+
+def plotsir(t, S, I, R):
+  f, ax = plt.subplots(1,1,figsize=(10,4))
+  ax.plot(t, S, 'b', alpha=0.7, linewidth=2, label='Susceptible')
+  ax.plot(t, I, 'y', alpha=0.7, linewidth=2, label='Infected')
+  ax.plot(t, R, 'g', alpha=0.7, linewidth=2, label='Recovered')
+
+  ax.set_xlabel('Time (days)')
+
+  ax.yaxis.set_tick_params(length=0)
+  ax.xaxis.set_tick_params(length=0)
+  ax.grid(visible=True, which='major', c='w', lw=2, ls='-')
+  legend = ax.legend()
+  legend.get_frame().set_alpha(0.5)
+  for spine in ('top', 'right', 'bottom', 'left'):
+      ax.spines[spine].set_visible(False)
+      #st.pyplot(f)
+      plt.show()
+
+plotsir(t, S, I, R)
