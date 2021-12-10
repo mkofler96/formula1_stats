9caf39085bea3d91398513b1fd40712dddc8faf8
mkofler96 <rasta.kof@gmail.com>
2021-12-10 17:36:34 +0100

    minor bugfixes

--- backend.py
+++ backend.py
@@ -13,9 +13,12 @@ def setup():
 
 
 def load_summary(year, circuit):
-    fp1 = create_summary(ff1.get_session(year, circuit, 'FP1'))
-    fp2 = create_summary(ff1.get_session(year, circuit, 'FP2'))
-    fp3 = create_summary(ff1.get_session(year, circuit, 'FP3'))
+    try:
+        fp1 = create_summary(ff1.get_session(year, circuit, 'FP1'))
+        fp2 = create_summary(ff1.get_session(year, circuit, 'FP2'))
+        fp3 = create_summary(ff1.get_session(year, circuit, 'FP3'))
+    except ff1.api.SessionNotAvailableError:
+        fp1, fp2, fp3 = [], [], []
     return fp1, fp2, fp3
 
 
@@ -42,9 +45,13 @@ def strfdelta(tdelta):
 
 def load_laps(year, circuit, fp1, fp2, fp3):
     print("loading laps")
-    fp1 = create_heat_map(ff1.get_session(year, circuit, 'FP1').load_laps(), fp1['Driver'])
-    fp2 = create_heat_map(ff1.get_session(year, circuit, 'FP2').load_laps(), fp2['Driver'])
-    fp3 = create_heat_map(ff1.get_session(year, circuit, 'FP3').load_laps(), fp3['Driver'])
+    try:
+        fp1 = create_heat_map(ff1.get_session(year, circuit, 'FP1').load_laps(), fp1['Driver'])
+        fp2 = create_heat_map(ff1.get_session(year, circuit, 'FP2').load_laps(), fp2['Driver'])
+        fp3 = create_heat_map(ff1.get_session(year, circuit, 'FP3').load_laps(), fp3['Driver'])
+    except ff1.api.SessionNotAvailableError:
+        fp1, fp2, fp3 = [], [], []
+
     return fp1, fp2, fp3
 
 
@@ -74,25 +81,25 @@ def get_current_weekends():
     print(type(weekends))
     current_season = weekends["MRData"]["RaceTable"]["@season"]
     #print(weekends["MRData"]["RaceTable"]["Race"])
-    races_left = list()
+    races_until_now = list()
     races = list()
     rounds = list()
     next_race_w_name = list()
     for race in weekends["MRData"]["RaceTable"]["Race"]:
-        print("Round: " + race["@round"] + race["RaceName"] + " at circuit: " + race["Circuit"]["@circuitId"] + " on " + race["Date"])
-        delta_to_now = datetime.strptime(race["Date"], '%Y-%m-%d') - datetime.today() + timedelta(days=1)
-        if delta_to_now > timedelta(days=0):
-            races_left.append(race["Circuit"]["@circuitId"])
+        #print("Round: " + race["@round"] + race["RaceName"] + " at circuit: " + race["Circuit"]["@circuitId"] + " on " + race["Date"])
+        delta_to_now = datetime.strptime(race["Date"], '%Y-%m-%d') - datetime.today()
+        if delta_to_now < timedelta(days=0):
+            races_until_now.append(race["@round"])
             next_race_w_name.append(race["RaceName"])
         rounds.append(race["@round"])
         races.append(race["RaceName"])
-    print("next Grand Prix: ")
-    print(races_left[0])
+    #print("next Grand Prix: ")
+    #print(races_until_now[-1])
     season = weekends["MRData"]["RaceTable"]["Race"][0]["@season"]
     race_names = races
-    next_race = races_left[0]
+    recent_race = int(races_until_now[-1])
     # todo: next_race could be dict containing name and short
-    return season, race_names, next_race, next_race_w_name[0]
+    return season, race_names, recent_race, next_race_w_name[0]
 
 def rest():
     laps = monza_quali.load_laps(with_telemetry=True)
--- main.py
+++ main.py
@@ -5,21 +5,26 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime as dt
+import math
 import seaborn as sns
 from backend import *
 
 
-season, race_names, next_race, current_weekend_title = get_current_weekends()
-year = season
-circuit = next_race
-
-setup()
-fp1, fp2, fp3 = load_summary(year, circuit)
-laps_fp1, laps_fp2, laps_fp3 = load_laps(year, circuit, fp1, fp2, fp3)
+season, race_names, recent_race, current_weekend_title = get_current_weekends()
 
 # layout
 st.set_page_config(layout="wide")
-st.markdown("<h1 style='text-align: center;'>"+current_weekend_title+"</h1>", unsafe_allow_html=True)
+race_choice = st.sidebar.selectbox("Select Race", race_names, index=(recent_race-1))
+st.write('You selected:', race_choice)
+year = season
+circuit = race_names.index(race_choice) + 1
+setup()
+fp1, fp2, fp3 = load_summary(year, circuit)
+print(circuit)
+print(type(circuit))
+laps_fp1, laps_fp2, laps_fp3 = load_laps(year, circuit, fp1, fp2, fp3)
+
+st.markdown("<h1 style='text-align: center;'>" + race_choice +"</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center;'> In-Depth Analysis by Michael Kofler</h3>", unsafe_allow_html=True)
 st.markdown("""---""")
 null, col_left_fp1, col_right_fp1, null2 = st.columns([0.5, 1, 2.5, 0.5])
@@ -51,14 +56,16 @@ col_left_fp3.dataframe(data = fp3, width=1024, height=768)
 f, ax = plt.subplots()
 plt.grid(visible=None)
 
-sns.heatmap(laps_fp1.transpose(), vmin=laps_fp1.min().min(), vmax=laps_fp1.min().max(), fmt="f", linewidths=.5, ax=ax, mask=laps_fp1.transpose().isnull(), cmap = "spring")
+print(type(laps_fp1.min()))
+max = laps_fp1.min()+2
+sns.heatmap(laps_fp1.transpose(), vmin=laps_fp1.min().min(), vmax=laps_fp1.min().min()*1.02, fmt="f", linewidths=.5, ax=ax, mask=laps_fp1.transpose().isnull(), cmap = "spring")
 col_right_fp1.pyplot(f)
 
 f2, ax2 = plt.subplots()
 
-sns.heatmap(laps_fp2.transpose(), vmin=laps_fp2.min().min(), vmax=laps_fp2.min().max(), fmt="f", linewidths=.5, ax=ax2, mask=laps_fp2.transpose().isnull(), cmap = "spring")
+sns.heatmap(laps_fp2.transpose(), vmin=laps_fp2.min().min(), vmax=laps_fp2.min().min()*1.02, fmt="f", linewidths=.5, ax=ax2, mask=laps_fp2.transpose().isnull(), cmap = "spring")
 col_right_fp2.pyplot(f2)
 
 f3, ax3 = plt.subplots()
-sns.heatmap(laps_fp3.transpose(), vmin=laps_fp3.min().min(), vmax=laps_fp3.min().max(), fmt="f", linewidths=.5, ax=ax3, mask=laps_fp3.transpose().isnull(), cmap = "spring")
+sns.heatmap(laps_fp3.transpose(), vmin=laps_fp3.min().min(), vmax=laps_fp3.min().min()*1.02, fmt="f", linewidths=.5, ax=ax3, mask=laps_fp3.transpose().isnull(), cmap = "spring")
 col_right_fp3.pyplot(f3)
